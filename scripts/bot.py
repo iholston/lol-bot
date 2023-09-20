16c54de23d293c44575f7c36c43859de7f25b699
Isaac Holston <isaac.holston@outlook.com>
2023-09-20 13:34:57 -0600

    hotfix #75 log filepath issue

--- scripts/bot.py
+++ scripts/bot.py
@@ -14,6 +14,8 @@ log = logging.getLogger(__name__)
 
 def main():
     log_dir = os.path.join(os.path.normpath(os.getcwd() + os.sep + os.pardir), 'logs')
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)     
     filename = os.path.join(log_dir, datetime.now().strftime('%d%m%Y_%H%M_log.log'))
 
     formatter = logging.Formatter(fmt='[%(asctime)s] [%(levelname)-7s] [%(funcName)-21s] %(message)s', datefmt='%d %b %Y %H:%M:%S')
