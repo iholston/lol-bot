474ae815bb09db704c65ffd11d5a93d55433af04
unknown <isaac.holston@outlook.com>
2023-09-20 07:57:02 -0600

    reformat

--- scripts/bot.py
+++ scripts/bot.py
@@ -1,3 +1,7 @@
+"""
+Where bot execution starts, sets logging configurations
+"""
+
 import logging
 import os
 import sys
@@ -6,12 +10,11 @@ from client import Client
 from datetime import datetime
 from logging.handlers import RotatingFileHandler
 
+
 def set_logs(log_dir, level=logging.INFO) -> None:
     """Sets log output to file and console"""
     filename = os.path.join(log_dir, datetime.now().strftime('%d%m%Y_%H%M_log.log'))
-
-    formatter = logging.Formatter(fmt='[%(asctime)s] [%(levelname)-7s] [%(funcName)-21s] %(message)s',
-                                  datefmt='%d %b %Y %H:%M:%S')
+    formatter = logging.Formatter(fmt='[%(asctime)s] [%(levelname)-7s] [%(funcName)-21s] %(message)s', datefmt='%d %b %Y %H:%M:%S')
 
     ch = logging.StreamHandler(sys.stdout)
     fh = RotatingFileHandler(filename=filename, maxBytes=500000, backupCount=1)
@@ -21,8 +24,8 @@ def set_logs(log_dir, level=logging.INFO) -> None:
 
     logging.getLogger().addHandler(ch)
     logging.getLogger().addHandler(fh)
-
     logging.getLogger().setLevel(level)
+
 
 if __name__ == '__main__':
     if not os.path.exists(constants.LEAGUE_CLIENT_DIR):
