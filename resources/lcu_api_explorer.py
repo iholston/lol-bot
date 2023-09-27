5fdda03ea45ce51c8af2787bc1e6239193b0cdbc
unknown <isaac.holston@outlook.com>
2023-09-27 08:28:22 -0600

    doc update

--- resources/lcu_api_explorer.py
+++ resources/lcu_api_explorer.py
@@ -27,10 +27,12 @@ ignore = ['/lol-game-data/assets',
 
 @connector.ready
 async def connect(_) -> None:
+    """Displays connection confirmation"""
     print("LCU API Connected")
 
 @connector.ws.register('/')
 async def event_listener(_, event) -> None:
+    """Prints event info"""
     for skip_text in ignore:
         if skip_text in event.uri:
             return
